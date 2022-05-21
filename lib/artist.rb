d9c49d2ebb52bb40bf2ffb7c9fc6888c4b82a592
Matthew <matthew_mcaleer@hotmail.com>
2022-05-21 20:08:25 +1000

    commit tree a4f03fe5f4e4d795ff31211132bda01276cedbcb

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+    attr_accessor :name
+
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.artist == self
+    end
+
+    def add_song
+    end
+
+end
